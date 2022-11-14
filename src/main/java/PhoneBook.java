3c69a83bdd0fbf4fcb19ce0d3c2ac0033f6844a1
Andrey Vasilyev <avasilyev.gsg@gmail.com>
2022-11-14 14:05:25 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    public Map<String, String> contactList = new HashMap<>();
+
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,42 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    PhoneBook sut = new PhoneBook();
+
+    @Test
+    public void addTest() {
+        //arrange
+        String name1 = "Petr";
+        String number1 = "89112222222";
+        String name2 = "Andrew";
+        String number2 = "89113333333";
+        int expected = 2;
+        //act
+        sut.add(name1, number1);
+        int result = sut.add(name2, number2);
+        //assert
+        assertEquals(expected, result);
+    }
+
+    @BeforeEach
+    public void initTest() {
+        System.out.println("Test started");
+    }
+
+    @AfterEach
+    public void finishTest() {
+        System.out.println("Test finished");
+    }
+
+    @BeforeAll
+    public static void startedTests() {
+        System.out.println("Tests started");
+    }
+
+    @AfterAll
+    public static void finishedTests() {
+        System.out.println("Tests finished");
+    }
 }
