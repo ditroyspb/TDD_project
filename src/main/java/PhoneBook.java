988e8d2d286e4cb8d8b21d8f6b4de385a800750e
Andrey Vasilyev <avasilyev.gsg@gmail.com>
2022-11-14 14:41:35 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -6,6 +6,11 @@ public class PhoneBook {
     public Map<String, String> contactList = new HashMap<>();
 
     public int add(String name, String number) {
-        return 0;
+        if (!contactList.containsValue(number)) {
+            contactList.put(name, number);
+        } else {
+            System.out.println("Данный контакт уже добавлен в телефонную книгу :(");
+        }
+        return contactList.size();
     }
 }
